bc14b80ab6fbff0e16b5e2d613b7ab8c73205d5e
Madeleine <maddepersoon@gmail.com>
2018-05-22 15:05:11 +0200

    test

--- api/input/index.js
+++ api/input/index.js
@@ -6,7 +6,7 @@ const router = require('express').Router();
 
 router.post('/employees', employeeRoutes.create);
 router.get('/employees', employeeRoutes.findAll);
-router.get('/employees/:employeeId', employeeRoutes.findOne);
+router.get('/employes/:employeeId', employeeRoutes.findOne);
 
 module.exports = {
   router
