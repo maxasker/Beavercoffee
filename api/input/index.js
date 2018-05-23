5faa200d3f0efac0279599d9f4b62c097394320f
Madeleine <maddepersoon@gmail.com>
2018-05-23 14:53:09 +0200

    minor fix

--- api/input/index.js
+++ api/input/index.js
@@ -7,7 +7,7 @@ const router = require('express').Router();
 router.post('/employees', employeeRoutes.create);
 router.get('/employees', employeeRoutes.findAll);
 router.get('/employees/:employeeId', employeeRoutes.findOne);
-router.put('/employees/:employeeId', employeeRoutes.updateOne);
+router.put('/employees/:employeeId', employeeRoutes.update);
 
 module.exports = {
   router
