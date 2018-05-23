8647fcfabd2f7b040b92ed3877611e087aa813f2
maxasker <maxasker@hotmail.com>
2018-05-23 13:26:46 +0200

    fix

--- models/index.js
+++ models/index.js
@@ -1,7 +1,5 @@
 const Employee = require('./employee.js');
-const Comment = require('./comment.js');
 const Customer = require('./customer.js');
-const History = require('./history.js');
 const Location = require('./location.js');
 const Menu = require('./menu.js');
 const Order = require('./order.js');
@@ -10,10 +8,8 @@ const Storage = require('./storage.js');
 const Store = require('./store.js');
 
 module.exports = {
-  Comment,
   Employee,
   Customer,
-  History,
   Location,
   Menu,
   Order,
