620aabb831e1c66ff45fa41f467df611eb8ba99b
maxasker <maxasker@hotmail.com>
2018-05-31 13:39:44 +0200

    menugrejer

--- api/controllers/menu.controller.js
+++ api/controllers/menu.controller.js
@@ -1,27 +1,51 @@
 'use strict';
 const models = require('../../models');
 const {ObjectId} = require('mongodb');
-const storeController = require('./store.Controller.js');
 
 function create () {
   const Menu = new models.Menu();
   return Menu.save();
 }
 
+function createMenuItem (data) {
+  const MenuItem = new models.MenuItem(data);
+  return MenuItem.save();
+}
+
+function linkToMenu (menuItemId, menuId) {
+  const updateData = {};
+  updateData['menu_items'] = menuItemId;
+  const pushObj = {
+    $push: updateData
+  };
+  return models.Menu.findOneAndUpdate(
+    {_id: ObjectId(menuId._id)},
+    pushObj,
+    {new: true}
+  );
+}
+
+function addToMenu (data, menuId) {
+  return createMenuItem(data)
+  .then(function (results) {
+    return linkToMenu(results._id, menuId)
+    .then(function (result) {
+      console.log(result);
+      return Promise.resolve(results);
+    });
+  });
+}
+
 function addMenuItem (data, storeId) {
   if (!ObjectId.isValid(storeId)) {
     return Promise.reject(new TypeError(`Invalid id: ${storeId}`));
   }
-  return storeController.findOneStore(ObjectId(storeId))
+  return models.Store.findById(ObjectId(storeId))
   .then(function (results) {
-    var pushObj = {
-      $push: data
-    };
-    return models.Store.findOneAndUpdate(
-      {_id: ObjectId(results._id)},
-      pushObj,
-      {new: true}
-    );
+    return addToMenu(data, results.menu)
+    .then(function (results) {
+      return Promise.resolve(results);
+    });
   });
 }
 
