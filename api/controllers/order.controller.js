2a7857d0f3a515bc32214f9a0195d4e749d5491c
Karolin Spång <karolinspang@Karolins-MacBook-Air.local>
2018-06-01 16:28:30 +0200

    beverages + customer

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -2,45 +2,69 @@
 const models = require('../../models');
 const menuController = require('./menu.controller.js');
 const customerController = require('./customerController.js');
-
 const {ObjectId} = require('mongodb');
 
-//OBS - OM VAR TIONDE KAFFE
 function create (data) {
-  let sum = 0.00;
+let sum = 0.00;
+let bev = 0; //antalet beverages som kunden har på sitt kort
+let nbrBev = 0; //antalet beverages som kunden lagt i denna beställning
 
   return customerController.getData(ObjectId(data.customer_id))
   .then(function (res){
-//      console.log('bvrg' + {res});
+    bev = parseInt(res.beverages);
+      var i;
+      for(i=0; i<data.items.length; i++){
+        nbrBev += parseInt(data.items[i].quantity);
+      }
 
-      return sumPrices(data.items, sum)
+      var j, max = 10;
+      for(j=1; j<=nbrBev; j++){
+            console.log(j);
+            bev += 1;
+            if(bev >= 10){
+              bev = 0;
+          }
+      }
+
+      return sumPrices(data.items, sum, parseInt(res.beverages))
       .then(function (newSum) {
-          console.log(newSum)
           data.price=newSum;
           const Order = new models.Order(data);
-          return Order.save();
+          return Order.save()
+              .then(function (res){
+              let orderId = res;
+              return customerController.updateBeverages(ObjectId(res.customer_id), bev).then(function(res){
+                return models.Order.findById(orderId);
+        })
       })
+    })
   });
 }
 
+function sumPrices (menu_items, sum, beverages) {
+var i=0;
 
-//hämta antalet beverages för vald kund
-/*function nbrBeverages (customerId){
-     return customerController.getData(ObjectId(customerId));
-        .then(function (res) {
-          console.log(res.beverages);
-         return
-     });
-}*/
-
-function sumPrices (menu_items, sum) {
   let allPromises = menu_items.map(function (item) {
         return menuController.getMenuItem(ObjectId(item.type))
         .then(function (res) {
-          console.log(parseFloat(res.price));
-          console.log(parseFloat(item.quantity));
-          sum = sum + (parseFloat(res.price)*parseFloat(item.quantity));
-        })
+            var discount = 0.00;
+            console.log(item.quantity);
+            console.log(res.price);
+
+            i += parseInt(item.quantity);
+            if((beverages+i) >= 10){
+                    discount = res.price;
+                    console.log(beverages + ' ' + i);
+                    console.log('dis:' + discount);
+                    beverages = 0;
+                    i = 0;
+                   }
+
+        //dra av ingridienser ?
+
+        //beräkna summa och dra av discount
+          sum = sum + (((parseFloat(res.price)*parseFloat(item.quantity)))-discount);
+        });
     })
     return Promise.all(allPromises)
     .then(function (){
