ff2051967e7fa617ca921d2e7c396dd9ad0951a0
Karolin Spång <karolinspang@Karolins-MacBook-Air.local>
2018-06-01 17:11:27 +0200

    småfix

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -12,6 +12,7 @@ let nbrBev = 0; //antalet beverages som kunden lagt i denna beställning
   return customerController.getData(ObjectId(data.customer_id))
   .then(function (res){
     bev = parseInt(res.beverages);
+
       var i;
       for(i=0; i<data.items.length; i++){
         nbrBev += parseInt(data.items[i].quantity);
@@ -19,7 +20,6 @@ let nbrBev = 0; //antalet beverages som kunden lagt i denna beställning
 
       var j, max = 10;
       for(j=1; j<=nbrBev; j++){
-            console.log(j);
             bev += 1;
             if(bev >= 10){
               bev = 0;
@@ -48,14 +48,10 @@ var i=0;
         return menuController.getMenuItem(ObjectId(item.type))
         .then(function (res) {
             var discount = 0.00;
-            console.log(item.quantity);
-            console.log(res.price);
 
             i += parseInt(item.quantity);
             if((beverages+i) >= 10){
                     discount = res.price;
-                    console.log(beverages + ' ' + i);
-                    console.log('dis:' + discount);
                     beverages = 0;
                     i = 0;
                    }
